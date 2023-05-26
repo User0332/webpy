e790d453ec983cdfe469e0eb7cf780c4a3c7a790
User0332 <carlzfurtado@gmail.com>
2023-05-26 19:20:09 -0400

    update docs again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,10 @@ release = 'latest'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
